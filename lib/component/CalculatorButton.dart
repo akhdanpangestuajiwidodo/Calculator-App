0258dce53b185e1be8b530dea43e0f36cd4634ca
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-03-05 19:20:09 +0700

    create CalculatorButton widget

---
+++ lib/component/CalculatorButton.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CalculatorButton extends StatelessWidget {
+  final Color backgroundColor;
+  final Color foregroundColor;
+  final String text;
+
+  CalculatorButton(
+      {required this.backgroundColor,
+      required this.foregroundColor,
+      required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: backgroundColor,
+      child: Text(
+        text,
+        style: Theme.of(context)
+            .textTheme
+            .headline4!
+            .copyWith(color: foregroundColor),
+      ),
+    );
+  }
+}
