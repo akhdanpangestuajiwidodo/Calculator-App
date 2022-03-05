9f9d728f0cabeaca5a5a9592b96b03e19e4eff70
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-03-05 19:27:39 +0700

    new constructor for value icon not text

--- lib/component/CalculatorButton.dart
+++ lib/component/CalculatorButton.dart
@@ -5,24 +5,35 @@ class CalculatorButton extends StatelessWidget {
   final Color backgroundColor;
   final Color foregroundColor;
   final String text;
+  IconData? icon;
 
   CalculatorButton(
       {required this.backgroundColor,
       required this.foregroundColor,
       required this.text});
 
+  CalculatorButton.Icon(
+      {required this.backgroundColor,
+      required this.foregroundColor,
+      required this.icon,
+      required this.text});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: backgroundColor,
-      child: Text(
-        text,
-        style: Theme.of(context)
-            .textTheme
-            .headline4!
-            .copyWith(color: foregroundColor),
-      ),
+      child: icon == null
+          ? Text(
+              text,
+              style: Theme.of(context)
+                  .textTheme
+                  .headline4!
+                  .copyWith(color: foregroundColor),
+            )
+          : Icon(
+              icon,
+              color: foregroundColor,
+            ),
     );
   }
 }
